7db14bb67dc8210144d0b23cd0a9b9693f23f564
acooma <acoozee@gmail.com>
2021-08-29 13:07:08 -0500

    small

--- src/components/stickyHeader.js
+++ src/components/stickyHeader.js
@@ -15,9 +15,9 @@ const StickyHeader = (props) => {
     const winScroll =
       document.body.scrollTop || document.documentElement.scrollTop;
 
-    const height =
-      document.documentElement.scrollHeight -
-      document.documentElement.clientHeight;
+    // const height =
+    //   document.documentElement.scrollHeight -
+    //   document.documentElement.clientHeight;
 
     // const scrolled = winScroll / height;
 
